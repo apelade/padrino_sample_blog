7feb55906e8450d3fa7a4422b6c9fa15d4f24b2e
Paul McCulloch <hrfeels@gmail.com>
2014-07-31 15:51:54 -0700

    heroku changes

--- config/database.rb
+++ config/database.rb
@@ -24,13 +24,12 @@ ActiveRecord::Base.configurations[:development] = {
 }
 
 ActiveRecord::Base.configurations[:production] = {
-  :adapter   => 'postgresql',
-  :database  => 'test_blog_production',
-  :username  => 'postgres',
-  :password  => 'postgres',
-  :host      => 'localhost',
-  :port      => 5432
-
+    :adapter  => 'postgresql',
+    :encoding => 'utf8',
+    :database => postgres.path[1..-1],
+    :username => postgres.user,
+    :password => postgres.password,
+    :host     => postgres.host
 }
 
 ActiveRecord::Base.configurations[:test] = {
